e89e9c29897d1b4519fe7042adc5949817bbd1a6
jielun13 <jielun13@gmail.com>
2013-03-08 18:28:53 +0800

    test and create routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,46 +2,30 @@ require 'spec_helper'
 
 describe "StaticPages" do
   
+  subject {page}
+  
   describe "Home page" do
-  	
-  	it "should have h1 'Watsup'" do
-  		visit '/static_pages/home'
-  		page.should have_selector('h1', :text => 'Watsup')
-  	end
-
-  	it "should have the title 'Watsup'" do
-  		visit '/static_pages/home'
-  		page.should have_selector('title', :text => 'Watsup')
-  	end
-
+  	before {visit root_path}
+  	it {should have_selector('h1', :text: 'Watsup')} 		
+  	it {should have_selector('title', :text => 'Watsup')}
   end
 
   describe "Help page" do
-  	
-  	it "should have the h1 'Help'" do
-  		visit '/static_pages/help'
-  		page.should have_selector('h1', :text => 'Help')
-  	end
-
-  	it "should have the title 'Help'" do
-  		visit '/static_pages/help'
-  		page.should have_selector('title', :text => 'Help')
-  	end
-
+  	before {visit help_path}
+  	it {should have_selector('h1', :text => 'Help')}
+  	it {should have_selector('title', :text => 'Help')}
   end 
 
   describe "About page" do
-  	
-  	it "should have the h1 'About'" do
-  		visit '/static_pages/about'
-  		page.should have_selector('h1', :text => 'About')
-  	end
-  
-  	it "should have title 'About'" do
-  		visit '/static_pages/about'
-  		page.should have_selector('title', :text => 'About')
-  	end
+  	before {visit about_path}
+  	it {should have_selector('h1', :text => 'About')}
+   	it {should have_selector('title', :text => 'About')}
+  end
 
+  describe "Contacts page" do
+    before {visit contact_path}
+    it {should have_selector('h1', text: 'Contact')}
+    it {should have_selector('title', text:'Contact')}
   end
 
 end
