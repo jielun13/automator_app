d64f6f4821884e5ceedf91208db0ee4f3abedfc9
jielun13 <jielun13@gmail.com>
2013-03-10 14:26:55 +0800

    finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,20 +6,20 @@ describe "StaticPages" do
   
   describe "Home page" do
   	before {visit root_path}
-  	it {should have_selector('h1', :text: 'Watsup')} 		
-  	it {should have_selector('title', :text => 'Watsup')}
+  	it {should have_selector('h1', text: 'Watsup')} 		
+  	it {should have_selector('title', text: 'Watsup')}
   end
 
   describe "Help page" do
   	before {visit help_path}
-  	it {should have_selector('h1', :text => 'Help')}
-  	it {should have_selector('title', :text => 'Help')}
+  	it {should have_selector('h1', text: 'Help')}
+  	it {should have_selector('title', text: 'Help')}
   end 
 
   describe "About page" do
   	before {visit about_path}
-  	it {should have_selector('h1', :text => 'About')}
-   	it {should have_selector('title', :text => 'About')}
+  	it {should have_selector('h1', text: 'About')}
+   	it {should have_selector('title', text: 'About')}
   end
 
   describe "Contacts page" do
